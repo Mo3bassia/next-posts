3d06d62e7d30233b1e02aeebb266cd252f226e16
Mohamed Abassia <mo3bassia@gmail.com>
2025-04-28 20:36:39 +0300

    handling errors that can be excuted when user submit the form without any data and show errors

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -9,6 +9,22 @@ export default function NewPostPage() {
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
+    let errors = [];
+
+    if (!title || title.trim().length == 0) {
+      errors.push("Title is required field");
+    }
+    if (!content || content.trim().length == 0) {
+      errors.push("Content is required field");
+    }
+    if (!image || image.size == 0) {
+      errors.push("Image is required field");
+    }
+    if (errors.length > 0) {
+      return {
+        errors,
+      };
+    }
 
     await storePost({
       title,
